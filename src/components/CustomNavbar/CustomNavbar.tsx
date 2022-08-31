3d230f0893796c95a4f9cd9f31dcae4ef2b8a8e9
ryu junho <ryujunho@ryus-MacBook-Pro.local>
2022-08-31 17:46:27 +0900

    2022-08-31 메인화면 코드정리

--- src/components/CustomNavbar/CustomNavbar.tsx
+++ src/components/CustomNavbar/CustomNavbar.tsx
@@ -1,61 +1,47 @@
-import { Input, Navbar } from '@nextui-org/react';
+import { Card, Input, Navbar, Spacer, Text, User } from '@nextui-org/react';
 import React, { useId, useState } from 'react';
 
-import { IMutationProps } from '@/shared/utils/common.type';
 import { Loading } from '@nextui-org/react';
-import QUERY_KEYS from '@/shared/apis/queryKeys';
-import { REQUESTS_OPERATION } from '@/shared/utils/common.constants';
 import { SearchIcon } from '@/components/SearchIcon/SearchIcon';
 import debounce from 'lodash/debounce';
 import { riotAPI } from '@/shared/apis/riot';
-import { useMutation } from 'react-query';
 import { useTheme as useNextTheme } from 'next-themes';
 import { useTheme } from '@nextui-org/react';
 
 const CustomNavbar = (): React.ReactElement => {
-  const { setTheme } = useNextTheme();
-  const { isDark, type } = useTheme();
-  const [summoner, setSummoner] = useState('');
+  // const { setTheme } = useNextTheme();
+  // const { isDark, type } = useTheme();
+  const [isLoading, setIsLoading] = useState(false);
+  console.log('isLoading: ', isLoading);
+  const [isListVisible, setIsListVisible] = useState(false);
+  const [summoner, setSummoner] = useState(null);
   const searchInputId = useId();
-  const {
-    mutate: searchSummonerMutate,
-    data,
-    isLoading,
-  } = useMutation((data: IMutationProps) => riotAPI.mutation(data), {
-    onMutate: ({ operation }) => ({
-      key: [QUERY_KEYS.SOMMONER],
-      onSuccess: (res, req: any) => {
-        console.log(res.data);
-        setSummoner(res.data);
-      },
-    }),
-  });
-  const handleSearchSummoner = debounce((e) => {
-    searchSummonerMutate({ operation: REQUESTS_OPERATION.GET, url: `/summoner/v4/summoners/by-name/${encodeURI(e.target.value)}` });
-  }, 1000);
 
-  if (isLoading) {
-    return (
-      <div className="flex-container justify-center items-center w-full h-full">
-        <Loading type="gradient" size="xl" color="secondary" />
-      </div>
-    );
-  }
+  const handleSearchSummoner = debounce((e) => {
+    if (e.target.value.length) {
+      setIsLoading(true);
+      riotAPI
+        .get(`/summoner/v4/summoners/by-name/${encodeURI(e.target.value)}`)
+        .then((res: any) => {
+          setSummoner(res);
+          setIsLoading(false);
+        })
+        .catch(() => setIsLoading(false))
+        .finally(() => setIsLoading(false));
+    }
+  }, 500);
   return (
     <Navbar
       isBordered
       variant="sticky"
       css={{
-        top: 'calc(50% - 38px)',
+        top: 'calc(50% - 60px)',
         background: 'transparent',
         border: 'none',
-        '& > .nextui-navbar-container': {
-          gap: 20,
-          height: 100,
-        },
+        '& > .nextui-navbar-container': { gap: 20, height: 120, flexWrap: 'wrap' },
       }}
     >
-      <Navbar.Brand hideIn="xs" css={{ '@xs': { w: '12%' } }}>
+      <Navbar.Brand hideIn="xs" css={{ width: '15%' }}>
         <div className="logo-wrapper logo-bg-animate">
           <svg className="logo" width="100%" height="100%" viewBox="0 0 565 140">
             <text x="30" y="90" fill="rgba(126, 58, 242, var(--bg-opacity)" fontSize="100" fontFamily="'Russo One'">
@@ -64,40 +50,59 @@ const CustomNavbar = (): React.ReactElement => {
           </svg>
         </div>
       </Navbar.Brand>
-      <Navbar.Content css={{ width: '100%', '@xsMax': { w: '80%', jc: 'space-between' } }}>
+      <Navbar.Content
+        css={{
+          width: '80%',
+          '@media (max-width: 650px)': {
+            width: '100%',
+          },
+        }}
+      >
         <Navbar.Item
           css={{
             width: '100%',
-            '@xsMax': { w: '100%', jc: 'center' },
-            '& .nextui-input-main-container': {
-              width: '100%',
-            },
+            '& .nextui-input-main-container': { width: '100%' },
           }}
         >
-          <Input
-            id={searchInputId}
-            underlined
-            labelPlaceholder="Search Summoner"
-            color="secondary"
-            clearable
-            contentLeft={<SearchIcon fill="var(--nextui-colors-accents6)" size={16} />}
-            contentLeftStyling={false}
-            css={{
-              width: '100%',
-              '@xsMax': { mw: '300px' },
-              '& .nextui-input-content--left': {
-                h: '100%',
-                ml: '$4',
-                dflex: 'center',
-                padding: 10,
-              },
-            }}
-            onChange={handleSearchSummoner}
-          />
+          <>
+            <Input
+              id={searchInputId}
+              underlined
+              labelPlaceholder="Search"
+              color="secondary"
+              clearable
+              contentLeft={<SearchIcon fill="var(--nextui-colors-accents6)" size={16} />}
+              contentLeftStyling={false}
+              css={{
+                width: '100%',
+                '@xsMax': { mw: '300px' },
+                '& .nextui-input-content--left': { h: '100%', ml: '$4', dflex: 'center', padding: 10 },
+              }}
+              onFocus={() => setIsListVisible(true)}
+              onClearClick={() => setIsListVisible(false)}
+              onChange={handleSearchSummoner}
+            />
+            <Card css={{ position: 'absolute', top: 50, overflowY: 'auto', maxHeight: 300, display: isListVisible ? 'block' : 'none' }}>
+              <Card.Body css={{ gap: 20 }}>
+                <Text h6>Result</Text>
+                {!summoner || isLoading ? (
+                  <div className="flex-container justify-center items-center w-full h-full">
+                    <Loading type="gradient" size="sm" color="secondary" />
+                  </div>
+                ) : (
+                  <User src={`https://opgg-static.akamaized.net/images/profile_icons/profileIcon${summoner.profileIconId}.jpg?image=q_auto&image=q_auto,f_png,w_auto&v=1661751970709`} name={`${summoner.name} (lv)${summoner.summonerLevel}`} size="lg" />
+                )}
+
+                <Spacer />
+                <Text h6>Recent Result</Text>
+                <User src="https://i.pravatar.cc/150?u=a042581f4e29026704d" name="Ariana Wattson" size="lg" />
+                <User src="https://i.pravatar.cc/150?u=a042581f4e29026704d" name="Ariana Wattson" size="lg" />
+                <User src="https://i.pravatar.cc/150?u=a042581f4e29026704d" name="Ariana Wattson" size="lg" />
+                <User src="https://i.pravatar.cc/150?u=a042581f4e29026704d" name="Ariana Wattson" size="lg" />
+              </Card.Body>
+            </Card>
+          </>
         </Navbar.Item>
-        {/* <Navbar.Item>
-        <Switch color="secondary" checked={isDark} onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')} />
-      </Navbar.Item> */}
       </Navbar.Content>
     </Navbar>
   );
